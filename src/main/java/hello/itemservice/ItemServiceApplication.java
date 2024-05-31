b2420051f27917e4fc489f7d42c100a8b4387203
jay sung <namic123@naver.com>
2024-05-31 17:54:07 +0900

    스프링 메세지 기본

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -2,6 +2,9 @@ package hello.itemservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class ItemServiceApplication {
@@ -10,4 +13,13 @@ public class ItemServiceApplication {
 		SpringApplication.run(ItemServiceApplication.class, args);
 	}
 
+	// .properties에 정의된 변수를 자동으로 불러와 메세지소스에 할당함.
+	// Spring boot에서 메시지소스를 기본적으로 빈으로 등록하므로, 아래와 같이 명시적으로 작성할 필요가 없음.
+//	@Bean
+//	public MessageSource messageSource(){
+//		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//		messageSource.setBasenames("messages", "errors");
+//		messageSource.setDefaultEncoding("utf-8");
+//		return messageSource;
+//	}
 }
